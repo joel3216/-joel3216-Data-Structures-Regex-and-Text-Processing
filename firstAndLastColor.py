4a4626fc116215074a9f6289b8c7d91bff4540da
joel3216 <joeljaxon3216@gmail.com>
2021-01-20 08:39:37 +0530

    ADD. program to print first and last element of the list

---
+++ firstAndLastColor.py
@@ -0,0 +1,7 @@
+'''
+AUTHOR: Joel Jackson
+DATE:19-01-2021
+DESCRIPTION:prints first and last element of the list 
+'''
+color_list=["Red","Green","White","Black"]
+print("First: "+color_list[0]+"\nLast: "+color_list[-1])
