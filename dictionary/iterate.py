6900c05a59e1b6178fc23f4eeca32e24c5dc7f46
joel3216 <joeljaxon3216@gmail.com>
2021-01-20 21:29:04 +0530

    ADD. program to iterate over dictionaries

---
+++ dictionary/iterate.py
@@ -0,0 +1,8 @@
+'''
+Author: Joel Jackson
+Date: 20-01-2021
+Description: program to iterate over dictionaries
+'''
+dictionary={1:10,2:20,3:30,4:40}
+for item in dictionary.items():
+    print(item) 
