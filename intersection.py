eda7ee132570ca20e2d0906268d664fda9a2ed84
joel3216 <joeljaxon3216@gmail.com>
2021-01-21 10:50:03 +0530

    REFACTOR. moved files to set folder

--- intersection.py
+++ intersection.py
@@ -5,9 +5,6 @@ Description: program to create an intersection of sets.
 '''
 set1={1,2,3,4,5}
 set2={1,3,5}
-intersection=set()
+intersection=set1.intersection(set2)
 
-for element in set1:
-    if element in set2:
-        intersection.add(element)
 print(intersection)
