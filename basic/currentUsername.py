76dde99aad71ab32a45038c8b6712ccd78833502
joel3216 <joeljaxon3216@gmail.com>
2021-01-21 10:15:55 +0530

    DELETE.removed files of other branches

--- basic/currentUsername.py
+++
@@ -1,8 +0,0 @@
-'''
-AUTHOR: Joel Jackson
-DATE:19-01-2021
-DESCRIPTION: prints the username 
-'''
-import subprocess
-
-print(subprocess.getoutput("env | grep USERNAME"))
--- basic/listFiles.py
+++
@@ -1,9 +0,0 @@
-'''
-AUTHOR: Joel Jackson
-DATE:19-01-2021
-DESCRIPTION: Program to print all files in the specified directory
-'''
-from subprocess import call
-
-directory=input("enter directory path")
-call(["ls", "-a",directory])
