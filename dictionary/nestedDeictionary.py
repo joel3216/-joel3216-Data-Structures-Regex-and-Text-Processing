4ae3097f9ee61e5addc650213974954995686a1a
joel3216 <joeljaxon3216@gmail.com>
2021-01-22 10:15:35 +0530

    REFACTOR. added user input along with dictionary as value for nested dictionary

--- dictionary/nestedDeictionary.py
+++ dictionary/nestedDeictionary.py
@@ -3,9 +3,13 @@ Author: Joel Jackson
 Date: 20-01-2021
 Description: program to convert a list into a nested dictionary of keys. 
 '''
-mylist=input("enter the list ").split(",")
-dictionary=current={}
-for element in mylist:
-    current[element]={}
-    current=current[element]
-print(dictionary)
+
+if __name__ == "__main__":
+    
+    mylist=input("enter the list ").split(",")
+    dictionary=current={}
+    for element in mylist:
+        value=input("enter the value")
+        current[element]=value,{}
+        current=current[element][1]
+    print(dictionary)
