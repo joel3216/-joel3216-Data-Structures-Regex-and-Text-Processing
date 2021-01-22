4c006b87469bc546193d568ccb1e6e2f3713a6ff
joel3216 <joeljaxon3216@gmail.com>
2021-01-22 08:45:25 +0530

    UPDATE. corrected syntaxerrors

--- tuple/createTuple.py
+++ tuple/createTuple.py
@@ -3,6 +3,10 @@ Author: Joel Jackson
 Date: 22-01-2021
 Description: program to create a tuple.
 '''
+
 if __name__ == "__main__":
-    myTuple=tuple(input("enter input values").split(","))
-    print(type(myTuple),myTuple)
+    try:
+        myTuple=tuple(input("enter input values").split(","))
+        print(type(myTuple),myTuple)
+    except ValueError:
+        print("invalid input")
