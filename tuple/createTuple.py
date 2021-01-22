d3ee2d314307b6538643114a99ad13f7ba931937
joel3216 <joeljaxon3216@gmail.com>
2021-01-22 08:41:49 +0530

    REFACTOR. added user input and try except block

--- tuple/createTuple.py
+++ tuple/createTuple.py
@@ -4,5 +4,5 @@ Date: 22-01-2021
 Description: program to create a tuple.
 '''
 if __name__ == "__main__":
-    myTuple=(1,2,3,4,5)
+    myTuple=tuple(input("enter input values").split(","))
     print(type(myTuple),myTuple)
